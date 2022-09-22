ec4e46c5aa35f107e272b067502e413351caaa1d
ogechi chukwu <ogechichukwu.ehirim@gmail.com>
2022-09-22 07:17:18 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+
+		putchar(c);
+
+		c = c + 1;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
