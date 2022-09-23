f0f9384ce59aef537ea5abc719ee676a7557a050
ogechi chukwu <ogechichukwu.ehirim@gmail.com>
2022-09-23 05:05:52 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
 
-	printf("size of a char: %1d byte(s)\n", sizeof(c));
-	printf("size of an int: %1d byte(s)\n", sizeof(i));
-	printf("size of a long int: %1d byte(s)\n", sizeof(li));
-	printf("size of a long long int: %1d byte(s)\n", sizeof(lli));
-	printf("size of a float: %1d byte(s)\n", sizeof(f));
+int main(void)
+
+{
+
+	int intType;
+
+	float floatType;
+
+	long int lType;
+
+	long long int llType;
+
+	char charType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(floatType));
+
+	printf("Size of a long int: %zu byte(s)\n", sizeof(lType));
+
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(llType));
+
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+
 }
