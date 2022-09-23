aeb0479fd7a7f10cfe96f7b1364ac71dfa1a505a
ogechi chukwu <ogechichukwu.ehirim@gmail.com>
2022-09-23 07:23:10 -0700

    MAIN.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,36 @@
+nt _putchar (char c);
+
+
+/*print_alphabet: print alphabets*/
+void print_alphabet (void);
+
+/*print_alphabet_x10: print alphabet 10x*/
+void print_alphabet_x10 (void);
+
+/*_islower: checks if a char is lower case*/
+int _islower(int c);
+
+/* _isalpha: checks if an argument is an alphabet*/
+int _isalpha(int c);
+
+/*print_sign: prints the sign of a num*/
+int print_sign(int n);
+
+/*_abs: prints the absolute value of an argument*/int _abs(int);
+
+/*print_last_digit: prints the last digit of a number*/
+
+int print_last_digit(int);
+
+/* print all minutes in a day*/
+void jack_bauer(void);
+
+/*print 9 times table*/
+void times_table(void);
+
+/*add two numbers*/
+int add(int, int);
+
+/* print from n to 98*/
+void print_to_98(int n);
+
