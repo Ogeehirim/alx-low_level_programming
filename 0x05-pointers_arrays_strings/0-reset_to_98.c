86c5be8048b2cfb6e2ec56cd3d9d01dd661ed16c
ogechi chukwu <ogechichukwu.ehirim@gmail.com>
2022-09-23 07:50:42 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - this function takes a pointer
  * and updates the data stored in the variable it is pointing to
  *@n: this holds the argument passed to the function
- *Return: nothing
+ * Return: nothing
  */
 
 void reset_to_98(int *n)
