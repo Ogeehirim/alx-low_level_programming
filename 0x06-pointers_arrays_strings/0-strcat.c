8626e5f620e00ba6f02643bc34a3a35377cb18e5
ogechi chukwu <ogechichukwu.ehirim@gmail.com>
2022-09-23 09:03:07 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,11 @@
 
 /**
  * _strcat - takes two strings and concatenates them
+ 
  * @dest: holds the first argument passed to the function
  * @src: holds the second string to be concatenated
  * Return: returns the concatenated text.
  */
-
 
 char *_strcat(char *dest, char *src)
 
